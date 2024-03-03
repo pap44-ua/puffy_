4a0ef7726974ec0864080881dd2444026e42acd4
mvr38-ua <mvr38@alu.ua.es>
2024-03-03 13:25:00 +0100

    Hacer canvas nevera y botones

---
+++ Puffy/Assets/Scripts/Cocina/Comida.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comida : MonoBehaviour
+{
+    public string nombre;
+    public int cantidad;
+    public Sprite imagen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
