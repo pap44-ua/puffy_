659848881d80c8390ba0f089e2092ec85f620c58
pap44-ua <pap44@alu.ua.es>
2024-04-06 12:52:12 +0200

    Se abren las pociones

---
+++ Puffy/Assets/Scripts/Pociones/quitarArmario.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitarArmario : MonoBehaviour
+{
+    public GameObject armario;
+    // Start is called before the first frame update
+
+    public void UwU()
+    {
+        armario.SetActive(false);
+    }
+    public void OnClick()
+    {
+        armario.SetActive(false);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
