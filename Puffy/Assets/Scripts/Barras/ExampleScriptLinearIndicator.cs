a2d9a3d752d1dbf0c7ca62400928833082447bca
Adrian Bartel <afbp2-ua@alu.ua.es>
2024-04-09 11:36:10 +0200

    Barras creadas a falta de la imagen con funcionalidad

---
+++ Puffy/Assets/Scripts/Barras/ExampleScriptLinearIndicator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScriptLinearIndicator : MonoBehaviour
+{
+
+    public LinearIndicator linearIndicator;
+    public LinearIndicator linearIndicator2, linearIndicator3, linearIndicator4;
+
+    public float minValue, maxValue;
+    public float currentValue;
+
+    void Start()
+    {
+        //Setup the linear indicator by code or do it in inspector
+        linearIndicator.SetupIndicator(0,100);
+        linearIndicator2.SetupIndicator(0, 100);
+        linearIndicator3.SetupIndicator(0, 100);
+        linearIndicator4.SetupIndicator(0, 100);
+
+
+        //linearIndicator.SetOrientation(LinearIndicator.Orientation.Horizontal);
+        //linearIndicator.reverse = false;
+        linearIndicator.SetValue(currentValue);
+        linearIndicator2.SetValue(currentValue);
+        linearIndicator3.SetValue(currentValue);
+        linearIndicator4.SetValue(currentValue);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+           //Aquí se pondrán los valores para ajustar las barras
+
+    }
+}
