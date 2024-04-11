c49860fcb59111c77c60be13d5ee56325c545777
aas141-ua <aas141@alu.ua.es>
2024-04-11 13:18:39 +0200

    Poner ropa a Puffy

--- Puffy/Assets/Scripts/Dormitorio/Dormitorio.cs
+++ Puffy/Assets/Scripts/Dormitorio/Dormitorio.cs
@@ -11,6 +11,22 @@ public class Dormitorio : MonoBehaviour
     public GameObject ojosCerrados;
     public GameObject zzz;
     public GameObject CanvasArmario;
+    public GameObject camisetaAdidas;
+    public GameObject camisetaNike;
+    public GameObject camisetaAzul;
+    public GameObject sudaderaAdidas;
+    public GameObject sudaderaNike;
+    public GameObject chaquetaAzul;
+    public GameObject chaquetaGris;
+    public GameObject gorroDormir;
+    public GameObject pantalonesLilas;
+    public GameObject pantalonesRojos;
+    public GameObject pulsera;
+    public GameObject tatuaje;
+    public GameObject vestidoLila;
+    public GameObject vestidoRojo;
+    public GameObject zapatillas;
+    public GameObject pantunflas;
 
     //public bool dormido = false;
 
@@ -23,6 +39,22 @@ public class Dormitorio : MonoBehaviour
         ojosCerrados.SetActive(!ojosCerrados.activeSelf);
         zzz.SetActive(!zzz.activeSelf);
         CanvasArmario.SetActive(!CanvasArmario.activeSelf);
+        camisetaAdidas.SetActive(false);
+        camisetaNike.SetActive(false);
+        camisetaAzul.SetActive(false);
+        sudaderaAdidas.SetActive(false);
+        sudaderaNike.SetActive(false);
+        chaquetaAzul.SetActive(false);
+        chaquetaGris.SetActive(false);
+        gorroDormir.SetActive(false);
+        pantalonesLilas.SetActive(false);
+        pantalonesRojos.SetActive(false);
+        pulsera.SetActive(false);
+        tatuaje.SetActive(false);
+        vestidoLila.SetActive(false);
+        vestidoRojo.SetActive(false);
+        zapatillas.SetActive(false);
+        pantunflas.SetActive(false);
 
     }
 
@@ -56,6 +88,87 @@ public class Dormitorio : MonoBehaviour
                             // Ejecuta la lógica específica para el objeto deseado
                             hit.collider.gameObject.SendMessage("mostrarArmario");
                         }
+                        if (hit.collider.gameObject == camisetaAdidas)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            camisetaAdidas.SetActive(!camisetaAdidas.activeSelf);
+                        }
+                        if (hit.collider.gameObject == camisetaNike)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            camisetaNike.SetActive(!camisetaNike.activeSelf);
+                        }
+                        if (hit.collider.gameObject == camisetaAzul)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            camisetaAzul.SetActive(!camisetaAzul.activeSelf);
+                        }
+                        if (hit.collider.gameObject == sudaderaAdidas)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            sudaderaAdidas.SetActive(!sudaderaAdidas.activeSelf);
+                        }
+                        if (hit.collider.gameObject == sudaderaNike)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            sudaderaNike.SetActive(!sudaderaNike.activeSelf);
+                        }
+                        if (hit.collider.gameObject == chaquetaAzul)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            chaquetaAzul.SetActive(!chaquetaAzul.activeSelf);
+                        }
+                        if (hit.collider.gameObject == chaquetaGris)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            chaquetaGris.SetActive(!chaquetaGris.activeSelf);
+                        }
+                        if (hit.collider.gameObject == gorroDormir)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            gorroDormir.SetActive(!gorroDormir.activeSelf);
+                        }
+                        if (hit.collider.gameObject == pantalonesLilas)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            pantalonesLilas.SetActive(!pantalonesLilas.activeSelf);
+                        }
+                        if (hit.collider.gameObject == pantalonesRojos)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            pantalonesRojos.SetActive(!pantalonesRojos.activeSelf);
+                        }
+                        if (hit.collider.gameObject == pulsera)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            pulsera.SetActive(!pulsera.activeSelf);
+                        }
+                        if (hit.collider.gameObject == tatuaje)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            tatuaje.SetActive(!tatuaje.activeSelf);
+                        }
+                        if (hit.collider.gameObject == vestidoLila)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            vestidoLila.SetActive(!vestidoLila.activeSelf);
+                        }
+                        if (hit.collider.gameObject == vestidoRojo)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            vestidoRojo.SetActive(!vestidoRojo.activeSelf);
+                        }
+                        if (hit.collider.gameObject == zapatillas)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            zapatillas.SetActive(!zapatillas.activeSelf);
+                        }
+                        if (hit.collider.gameObject == pantunflas)
+                        {
+                            // Ejecuta la lógica específica para el objeto deseado
+                            pantunflas.SetActive(!pantunflas.activeSelf);
+                        }
+
                     }
                 }
             }
