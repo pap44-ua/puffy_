37d56861ce3fcf6369cd4b1b525503546e5f7e56
mdl92 <128229887+mdl92@users.noreply.github.com>
2024-04-15 15:52:01 +0200

    Jardin modificado

--- Puffy/Assets/Scripts/Jardin/juegos.cs
+++ Puffy/Assets/Scripts/Jardin/juegos.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class juegos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject ahorcado;
+    public GameObject ppt;
+    public GameObject memoria;
+    public GameObject tresRaya;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnMouseDown(){
+        if(gameObject == ahorcado){
+            SceneManager.LoadScene("Ahorcado");
+        }
+        else if(gameObject == ppt){
+            SceneManager.LoadScene("PPT_MainMenu");
+        }
+        else if(gameObject == memoria){
+            Debug.Log("Ha elegido memoria");
+        }
+        else if(gameObject == tresRaya){
+            SceneManager.LoadScene("Tres_en_raya");
+        }
+        else{
+            Debug.Log("No se ha encontrado el juego");
+        }
     }
 }
