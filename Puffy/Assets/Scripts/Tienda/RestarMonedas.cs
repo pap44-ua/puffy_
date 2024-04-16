bf62e15612adae1e8ecc20a45c0b767704a31dc1
jao9-ua <jao9@alu.ua.es>
2024-04-16 21:10:04 +0200

    cambios dinero

--- Puffy/Assets/Scripts/Tienda/RestarMonedas.cs
+++ Puffy/Assets/Scripts/Tienda/RestarMonedas.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Diagnostics;
 
 public class RestarMonedas : MonoBehaviour
 {
-
-    public TMP_Text textoMonedas;
-    public int cantidad;
-    // Start is called before the first frame update
-    void OnMouseDown()
-    {
-
-    }
+    public TMP_Text coinsText; // Referencia al componente TextMeshPro que muestra las monedas
+    public int coins = 1500; // Valor inicial de las monedas
 
     void Start()
     {
-        
+        // Actualiza el texto del componente TextMeshPro con el valor inicial de las monedas
+        UpdateCoinsText();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Método para comprar un objeto. Recibe el costo del objeto como parámetro.
+    public void BuyObject(int objectCost)
     {
-        
+        // Verifica si tienes suficientes monedas para comprar el objeto
+        if (coins >= objectCost)
+        {
+            // Resta el costo del objeto del total de monedas
+            coins -= objectCost;
+
+            // Actualiza el texto del componente TextMeshPro con el nuevo valor de las monedas
+            UpdateCoinsText();
+        }
+        else
+        {
+        }
+    }
+
+    // Método para actualizar el texto del componente TextMeshPro con el valor actual de las monedas
+    void UpdateCoinsText()
+    {
+        coinsText.text = "x" + coins.ToString();
     }
 }
