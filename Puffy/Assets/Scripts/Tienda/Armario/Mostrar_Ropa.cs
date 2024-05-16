abf55425073c64e0a21f5b9132d46ad7c7f06813
jao9-ua <jao9@alu.ua.es>
2024-05-16 20:46:52 +0200

    monedas

--- Puffy/Assets/Scripts/Tienda/Armario/Mostrar_Ropa.cs
+++ Puffy/Assets/Scripts/Tienda/Armario/Mostrar_Ropa.cs
@@ -6,13 +6,14 @@ public class Mostrar_Ropa : MonoBehaviour
 {
     public GameObject canvasRopa; // El Canvas de la nevera que quieres mostrar
     public GameObject canvasMenu;
+    public GameObject Atras;
 
     public void OnMouseDown()
     {
         // Invierte el estado de activaci�n del Canvas de la nevera
         canvasRopa.SetActive(true);
         canvasMenu.SetActive(false);
-
+        Atras.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
