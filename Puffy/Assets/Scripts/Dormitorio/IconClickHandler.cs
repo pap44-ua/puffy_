6ee666b8c0eac5261437bb56658c913c87fc70e9
pap44-ua <pap44@alu.ua.es>
2024-05-16 15:09:20 +0200

    arreglao

--- Puffy/Assets/Scripts/Dormitorio/IconClickHandler.cs
+++ Puffy/Assets/Scripts/Dormitorio/IconClickHandler.cs
@@ -9,30 +9,17 @@ public class IconClickHandler : MonoBehaviour
     public GameObject ojosAbiertos;
     public GameObject ojosCerrados;
     public GameObject zzz;
-
-    public LinearIndicator energia;
-    public float energiaBajaThreshold = 25f;
     public GameObject ojosCansados;
     //private bool estaDormido = false;
 
     void Start()
     {
-        
+
     }
 
-    void Update() {
-        // Verificar si la energía es baja y cambiar los ojos
+    void Update()
+    {
 
-        if (energia.GetValue() <= energiaBajaThreshold)
-        {
-            ojosAbiertos.SetActive(false);
-            ojosCansados.SetActive(true);
-        }
-        else
-        {
-            ojosAbiertos.SetActive(true);
-            ojosCansados.SetActive(false);
-        }
     }
 
     public void OnMouseDown()
@@ -40,11 +27,20 @@ public class IconClickHandler : MonoBehaviour
         fondoOscuro.SetActive(!fondoOscuro.activeSelf);
         iconoDormir.SetActive(!iconoDormir.activeSelf);
         iconoDespierto.SetActive(!iconoDespierto.activeSelf);
-        
-        //ojosAbiertos.SetActive(false);
+
+        if (ojosAbiertos.activeSelf)
+        {
+            ojosAbiertos.SetActive(!ojosAbiertos.activeSelf);
+        }
         ojosCerrados.SetActive(!ojosCerrados.activeSelf);
-        //ojosCansados.SetActive(!ojosCansados.activeSelf);
+        if (ojosCansados.activeSelf)
+        {
+            ojosCansados.SetActive(!ojosCansados.activeSelf);
+        }
         zzz.SetActive(!zzz.activeSelf);
+        iconoDormir.SendMessage("durmiendo");
 
     }
+
+
 }
