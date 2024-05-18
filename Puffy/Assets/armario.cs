d86eb7f2eef437d79d0ab637cd614cc038abce39
aas141-ua <aas141@alu.ua.es>
2024-05-18 10:36:06 +0200

    DORMITORIO: Añadida script contadorRopa y Armario

--- Puffy/Assets/armario.cs
+++ Puffy/Assets/armario.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class armario : MonoBehaviour
+public class Armario : MonoBehaviour
 {
+    public static int cantidadCamisetaAdidas = 0;
+    public static int cantidadCamisetaNike = 0;
+    public static int cantidadCamisetaAzul = 0;
+    public static int cantidadSudaderaAdidas = 0;
+    public static int cantidadSudaderaNike = 0;
+    public static int cantidadPantalonesRojos = 0;
+    public static int cantidadPantalonesLilas = 0;
+    public static int cantidadVestidoLila = 0;
+    public static int cantidadVestidoRojo = 0;
+    public static int cantidadChaquetaAzul = 0;
+    public static int cantidadChaquetaGris = 0;
+    public static int cantidadZapatillas = 0;
+    public static int cantidadZapatillasCasa = 0;
+    public static int cantidadGorroDormir = 0;
+    public static int cantidadPulsera = 0;
     // Start is called before the first frame update
     void Start()
     {
