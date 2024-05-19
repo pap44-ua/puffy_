d97ec015f942eba3f8230cf3b95ff862f3d51df0
Adrian Bartel <afbp2-ua@alu.ua.es>
2024-05-19 18:58:35 +0200

    avances

--- Puffy/Assets/Scripts/Barras/ExampleScriptLinearIndicator.cs
+++ Puffy/Assets/Scripts/Barras/ExampleScriptLinearIndicator.cs
@@ -9,29 +9,60 @@ public class ExampleScriptLinearIndicator : MonoBehaviour
 
     public float minValue, maxValue;
     public float currentValue;
-
+    public int valComida, valEnergia, valSalud, valDiversion;
     void Start()
     {
-        //Setup the linear indicator by code or do it in inspector
         comida.SetupIndicator(0,100);
         energia.SetupIndicator(0, 100);
         salud.SetupIndicator(0, 100);
         diversion.SetupIndicator(0, 100);
+        if (!PlayerPrefs.HasKey("Diversion") && !PlayerPrefs.HasKey("Comida") && !PlayerPrefs.HasKey("Energia") && !PlayerPrefs.HasKey("Salud"))
+        {
+            PlayerPrefs.SetInt("Comida", 10);
+            PlayerPrefs.SetInt("Energia", 10);
+            PlayerPrefs.SetInt("Salud", 10);
+            PlayerPrefs.SetInt("Diversion", 10);
+        }
 
 
-        //linearIndicator.SetOrientation(LinearIndicator.Orientation.Horizontal);
-        //linearIndicator.reverse = false;
-        comida.SetValue(50);
-        diversion.SetValue(75);
-        energia.SetValue(25);
-        salud.SetValue(40);
+        comida.SetValue(PlayerPrefs.GetInt("Comida"));
+        diversion.SetValue(PlayerPrefs.GetInt("Diversion"));
+        energia.SetValue(PlayerPrefs.GetInt("Energia"));
+        salud.SetValue(PlayerPrefs.GetInt("Salud"));
 
     }
 
-    // Update is called once per frame
+    public void modificarBarra(string nombre)
+    {
+        switch (nombre)
+        {
+            case "Comida":
+                comida.SetValue(PlayerPrefs.GetInt("Comida"));
+                break;
+            case "Diversion":
+                diversion.SetValue(PlayerPrefs.GetInt("Diversion"));
+                break;
+            case "Energia":
+                energia.SetValue(PlayerPrefs.GetInt("Energia"));
+                break;
+            case "Salud":
+                salud.SetValue(PlayerPrefs.GetInt("Salud"));
+                break;
+        }
+    }
+
     void FixedUpdate()
     {
-           //Aquí se pondrán los valores para ajustar las barras
-
+        if(comida.GetValue() != PlayerPrefs.GetInt("Comida"))
+            PlayerPrefs.SetInt("Comida", (int) comida.GetValue());
+        
+        if (diversion.GetValue() != PlayerPrefs.GetInt("Diversion"))
+            PlayerPrefs.SetInt("Diversion", (int)diversion.GetValue());
+     
+        if (salud.GetValue() != PlayerPrefs.GetInt("Salud"))
+            PlayerPrefs.SetInt("Salud", (int)salud.GetValue());
+        
+        if (energia.GetValue() != PlayerPrefs.GetInt("Energia"))
+            PlayerPrefs.SetInt("Energia", (int)energia.GetValue());
     }
 }
