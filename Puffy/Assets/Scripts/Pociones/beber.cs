c2b0be92836a429c4f54f84de55cd0686abc78f7
pap44-ua <pap44@alu.ua.es>
2024-05-19 12:08:12 +0200

    uwu

--- Puffy/Assets/Scripts/Pociones/beber.cs
+++ Puffy/Assets/Scripts/Pociones/beber.cs
@@ -11,14 +11,12 @@ public class beber : MonoBehaviour
     public LinearIndicator energia;
     public AudioSource audioUwU;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        // Desactiva la moneda al inicio del juego
+    
         moneda.SetActive(false);
 
-        // Obtén el componente Animator de la moneda
-        //monedaAnimator = moneda.GetComponent<Animator>();
     }
 
     public void OnMouseDown()
@@ -37,14 +35,7 @@ public class beber : MonoBehaviour
             Debug.Log("UWUUUUUUUUUUUUU");
         }
 
-        // Si hay un Animator adjunto a la moneda, inicia la animación
-        /*if (monedaAnimator != null)
-        {
-            monedaAnimator.Play("rotateCoin", 0, 0); // Reemplaza "rotateCoin" con el nombre de tu animación
-        }*/
-
-        // Desactiva la moneda después de un retraso de 2 segundos
-        //Invoke("DesactivarMoneda", 1f);
+       
         this.gameObject.SetActive(false);
         switch (gameObject.name)
         {
@@ -64,14 +55,13 @@ public class beber : MonoBehaviour
 
     }
 
-    // Método para desactivar la moneda
+
     private void DesactivarMoneda()
     {
         bocabuena.SetActive(true);
         moneda.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
